f35086502ac6a5ee24bdb867c542f61cce3371e4
mootqns <mqn360@gmail.com>
2022-11-02 19:29:27 -0700

    page structure

--- script.js
+++ script.js
@@ -2,3 +2,13 @@
 // Course: CPSC332
 // Assignment: Web Project Deliverable 2
 // Last Modifed: 11.04.2022
+
+window.addEventListener("DOMContentLoaded", domLoaded);
+
+function domLoaded(){
+    const startBtn = $("#play-btn")
+    startBtn.on("click", ()=>{
+        console.log("click!")
+    })
+    
+}
