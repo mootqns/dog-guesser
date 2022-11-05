d5235438b3df2938d0c38ded2f0e88ddd72f6951
mootqns <mqn360@gmail.com>
2022-11-05 14:00:23 -0700

    leaderboard page live

--- docs/finish/finish.js
+++ docs/finish/finish.js
@@ -8,41 +8,10 @@ window.onload = function() {
     const saveScoreBtn = document.getElementById("save-score-btn");
     const finalScore = document.getElementById("final-score");
     const recentScore = localStorage.getItem("recentScore");
-    // const profileImage = document.getElementById("profile-image");
-    // const imageUpload = document.getElementById("image-upload");
-
-    // add savescore event listener
-    const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
-    const MAX_SCORES = 4;
 
     finalScore.innerText = recentScore;
 
     user.addEventListener('keyup', () => {
         saveScoreBtn.disabled = !user.value;
     });
-
-
-    saveScoreBtn.addEventListener("click", saveHighScore(e));
-
-    function saveHighScore(e){
-        e.preventDefault();
-
-        const score = {
-            score: recentScore,
-            name: username.value
-        }
-
-        highScores.push(score);
-        highScores.sort((a,b) => {
-            return b.score - a.score;
-        });
-
-        highScores.splice(5);
-
-        localStorage.setItem("highscores", JSON.stringify(highScores));
-        window.location.assign("/");
-
-    }
-
-
 }
--- docs/index/index.js
+++ docs/index/index.js
@@ -7,9 +7,15 @@
 
 window.onload = function() {
     const startBtn = document.getElementById("play-btn");
+    const leaderboardBtn = document.getElementById("leaderboard-btn");
     
     // start-game button active
     startBtn.addEventListener("click", ()=>{
         window.location.assign('../game/game.html');
+    });
+
+    // leaderboard button active
+    leaderboardBtn.addEventListener("click", ()=>{
+        window.location.assign('../leaderboard/leaderboard.html');
     });
 }
